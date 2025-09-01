a4ab6de35bd0dba4a2b57f434f1e0108fc410992
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2025-09-01 14:42:32 -0400

    update config docstrings

--- config.go
+++ config.go
@@ -14,18 +14,41 @@ server: 0.0.0.0
 `
 
 type Config struct {
-	Name            string
-	Target          string
-	Registry        string
-	Email           string
-	AuthFile        string
-	Server          string
-	Platform        string
-	Volumes         []string
-	Hostname        string
-	Web             bool
+	// name of the application/container, must be unique per remote host (required)
+	Name string
+
+	// build target for docker container if needed, passed during the docker build process as --target (optional)
+	Target string
+
+	// container registry url to use (required)
+	Registry string
+
+	// email to use for tls certificate notifications. set to a dummy value if not supplied (optional)
+	Email string
+
+	// auth config.json for registry, will be copied to remote host. must be the same for all containers on a single host (required)
+	AuthFile string
+
+	// ip address of remote host server. the deployment machine must have ssh access (required)
+	Server string
+
+	// platform to build containers for, must match remote host. defaults to linux/amd64 (optional)
+	Platform string
+
+	// any additional volumes to mount on the remote host, follows docker convention (optional)
+	Volumes []string
+
+	// hostname to use for web applications and tls certs (optional, required if web is true)
+	Hostname string
+
+	// whether or not the application is a web service. if true, must expose port 80 from the docker container and specify a hostname
+	Web bool
+
+	// environment variable file (optional)
 	EnvironmentFile string
-	BuildArgFile    string
+
+	// docker build argument file (optional)
+	BuildArgFile string
 }
 
 func loadConfig(configKey string) (*Config, error) {
