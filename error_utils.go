fb6099e3b186751b0d96b47a6d828821bc3ed8e6
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2025-10-18 07:57:07 -0400

    better error message and os exit on error

--- error_utils.go
+++ error_utils.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func printConsoleError(message string, err error) {
 	fmt.Println()
-	fmt.Printf("\n----- error -----\n%s\nreason: %s\n\n*if this error is not clear, check the trace above to see which command failed*\n", message, err)
+	fmt.Printf("\n----- error -----\n%s\n\n----- reason -----\n%s\n\n*if this error is not clear, check the trace above to see which command failed*\n\n", message, err)
+	os.Exit(1)
 }
